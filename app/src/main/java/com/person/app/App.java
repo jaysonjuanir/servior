25c9142e64501a37bdd8c62177ff3297d8b9603e
Jayson Juanir <jaysonjuanir@yahoo.com>
2016-06-13 14:44:11 +0800

    Initialized the pom with all of dependencies

---
+++ app/src/main/java/com/person/app/App.java
@@ -0,0 +1,13 @@
+package com.person.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
